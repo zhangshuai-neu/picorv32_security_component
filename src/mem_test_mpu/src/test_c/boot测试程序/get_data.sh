fc639f3a5b47a18ab22fd2458f9032e96afdc6a6
ShuaiZhang <zhangshuaiisme@gmail.com>
2018-11-13 15:57:48 +0800

    some mod

--- src/mem_test_mpu/src/test_c/boot测试程序/get_data.sh
+++ src/mem_test_mpu/src/test_c/boot测试程序/get_data.sh
@@ -3,3 +3,8 @@
 line_num=`wc -l boot.asm |cut -d' ' -f1`
 let data_line_num=line_num-7
 tail boot.asm -n$data_line_num |cut -b 7-15 >boot.data
+
+cut boot.data -b 1-2 >boot1.data
+cut boot.data -b 3-4 >boot2.data
+cut boot.data -b 5-6 >boot3.data
+cut boot.data -b 7-8 >boot4.data
